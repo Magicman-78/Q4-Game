4d440de0f9069ffc05f431025a41c5de4f6d6680
KRESTONS-PC\ABEL4 <abel4prezz@gmail.com>
2025-05-08 16:05:00 -0700

    <restart the clock>

--- Assets/Kreston/Scripts/MaskUI.cs
+++ Assets/Kreston/Scripts/MaskUI.cs
@@ -1,16 +1,15 @@
 using UnityEngine;
 using UnityEngine.Events;
+using System.Collections;
 
 public class MaskUI : MonoBehaviour
 {
     public UnityEvent LeafTrigger;
     public UnityEvent SunTrigger;
     public UnityEvent MoonTrigger;
-
-
-
-    void Update()
+    
+    public void CheckMask()
     {
-        
+
     }
 }
